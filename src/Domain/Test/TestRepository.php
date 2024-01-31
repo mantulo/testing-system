1346716a95e77ec43ea1f5f218f8308692c989ae
mantulo <mantulo.vladimir@gmail.com>
2024-01-31 22:46:45 +0600

    Add missing exception to test repository definition

--- src/Domain/Test/TestRepository.php
+++ src/Domain/Test/TestRepository.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace App\Domain\Test;
 
+use App\Domain\Test\Exception\CouldNotGetTest;
+
 interface TestRepository
 {
     public function nextId(): TestId;
 
+    /**
+     * @param TestId $id
+     *
+     * @return Test
+     *
+     * @throws CouldNotGetTest
+     */
     public function byId(TestId $id): Test;
 
     public function findLast(): ?Test;
